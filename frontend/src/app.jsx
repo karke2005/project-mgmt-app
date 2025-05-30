371c7bbf0bd164d0a8ba2d3ccdd5428651b5de49
karke2005 <karthikeyans0108@gmail.com>
2025-05-30 13:53:18 +0530

    Create app.jsx

---
+++ frontend/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Project Management App</h1>
+      <p>Welcome to your project management platform.</p>
+    </div>
+  );
+}
+
+export default App;
